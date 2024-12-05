ddc485ecdff7d67318d1aafdb26a0a38dbce0bb2
UglyMiniKid <sai.memegod@gmail.com>
2024-12-05 11:34:56 +0700

    kto prochital tot v kletky s shokoladkami popal (zashita ot kosti)

--- main.py
+++ main.py
@@ -1,182 +1,46 @@
-"""number1=int(input("Ведите число: "))
-number2=0
-number3=0
-if number1%5==0:
-    number2=1
-if number1%3==0:
-    number3=1
-if number2==1 and number3==1:
-    print("Число",number1,"делится на 3 и на 5 без остатка.")
-elif number2==0 and number3==1:
-    print("Число", number1, "делится на 3 без остатка, а на 5 нет.")
-elif number2==1 and number3==0:
-    print("Число", number1, "делится на 5 без остатка, а на 3 нет.")
-else:
-    print("Число",number1,"не делится на 3 и на 5 без остатка.")
-number=int(input("Ведите число: "))
-if number%5==0 and number%3==0:
-    print("Импо")
-n=int(input("Скока у тебя баллов, бездырь? "))
-if n<=100 and n>=90:
-    print("Отлично")
-elif n<=69 and n>=50:
-    print("Удовлетворительно")
-elif n<=89 and n>=70:
-    print("Хорошо")
-else:
-    print("Не удовлетворительно")
-n1=int(input("Введите перваое число: "))
-n2=int(input("Введите vtoroe число: "))
-znak=input("Введите знак: ")
-if znak=="+":
-    print(n1+n2)
-elif znak=="-":
-    print(n1-n2)
-elif znak=="*":
-    print(n1*n2)
-elif znak=="/":
-    print(n1/n2)
-else:
-    print("Неверный знак")
-a=int(input("Введите первую сторону: "))
-b=int(input("Введите вторую сторону: "))
-c=int(input("Введите третью сторону: "))
-if a+b>c:
-    print("Треугольник существует")
-else:
-    print("Треугольник не существует")
-i=0
-n=int(input("Ведите каличества звездачек: "))
-for i in range(n):
-    print("*"*i)
-    i+=1
-i=0
-while i!=5:
-    if i==0 or i==4:
-        print("*****")
-        i+=1
-    elif i>0 and i<5:
-        print("*"," ","*")
-        i+=1
-print("Всо, полый квадрат тута")
+def analyze_text(text):
+    text = ''.join(char for char in text if char.isalpha() or char.isspace()).lower()
+    words = text.split()
+    word_counts = {}
+    for word in words:
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+    sorted_words = sorted(word_counts.items(), key=lambda item: (-item[1], item[0]))
+    return sorted_words
+def display_result(sorted_words):
+    for word, count in sorted_words:
+        print(f"{word}: {count}")
+if __name__ == "__main__":
+    input_text = input("Введите текст для анализа: ")
+    result = analyze_text(input_text)
+    display_result(result)
+#задачо номир 2
+def are_anagrams(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    sorted_str1 = sorted(list(str1))
+    sorted_str2 = sorted(list(str2))
+    return sorted_str1 == sorted_str2
+if __name__ == "__main__":
+    str1 = input("Введите первую строку: ")
+    str2 = input("Введите вторую строку: ")
 
-i=0
-n=int(input("Укажити размер треугольнека: "))+1
-while i!=n:
-    print("*"*i)
-    i+=1
-print("Всо, треугальник гатоф")
-i=0
-n=int(input("Укажити размер треугольнека: "))+1
-while i!=n:
-    if i<=2 or i==n-1:
-        print("*"*i)
-        i+=1
-    elif i>2 and i<=n-2:
-        print("*"," "*(i-3),"*")
-        i+=1
-    elif i==n-1:
-        print("*"*(i+3))
-print("Рот шатал этого")
-n=int(input("crjrf 'nb[ cfvs[ eltn& :"))
-for i in range(0,n+1):
-    for j in range(0,n-i):
-        print(end=" ")
-    for j in range(0,i):
-        print("*",end=" ")
-    print()
-if i==n:
-    for i in range(n-1,0,-1):
-        for j in range(0,n-i):
-            print(end=" ")
-        for j in range(0,i):
-            print("*",end=" ")
-        print()
-a = b = 1
-lst = [a, b]
-for i in range (2, 10):
-    c = a+b
-    lst += [c]
-    a = b
-    b = c
-print(*lst)
-
-
-from random import randint
-
-minimum = int(input('Минимум: '))
-maximus = int(input('Максимум: '))
-elementi = int(input('Количество элементов: '))
-
-lst = [randint(minimum, maximus) for i in range(elementi)]
-
-s = set(lst)
-for i in s:
-    print(f"'{i}': {lst.count(i)}")
-input_string=str(input("Строка"))
-def count_vowels(input_string):
-    vowels="аеёиоуюыэя"
-    count=0
-    for char in input_string.lower():
-        if char in vowels:
-            count+=1
-print("Количество гласных:", count_vowels(input_string))
-#задача 2
-def rotate(input_string):
-    words=input_string.split()
-    rotated=words[::-1]
-    return(rotated)
-input_string=input("Введите строку: ")
-result=rotate(input_string)
-print(result)
-#задачо номир 3
-def find(input_string, search):
-    if search in input_string:
-        return"Есть такое"
+    if are_anagrams(str1, str2):
+        print("Эти строки являются анаграммами.")
     else:
-        return"Нет такога"
-print('ДА' if word in str.lower() else 'НЕТ')
-user_input = str(input())
-output = user_input.replace(' ','_')
-print(output)
-str = input('Введите строку: ')
-word= input('Введите слово: ')
-
-
-print('ДА' if word in str.lower() else 'НЕТ')
-user_input = str(input())
-output = user_input.replace(' ','_')
-print(output)
-
-user_input = input("Введите строку: ")
-
-user_input = input("Введите строку: ")
-
-
-if user_input == user_input[::-1]:
-    print("ЭТА СТРОКА ПАЛИНДРОМ")
-else:
-    print ("ЭТА СТРОКА НЕ ЯВЛЯЕТСЯ ПАЛИНДРОМОМ")"""
-
-str = input('Введите строку: ')
-word= input('Введите слово: ')
-
-
-print('ДА' if word in str.lower() else 'НЕТ')
-user_input = str(input())
-output = user_input.replace(' ','_')
-print(output)
-
-user_input = input("Введите строку: ")
-
-
-if user_input == user_input[::-1]:
-    print("ЭТА СТРОКА ПАЛИНДРОМ")
-else:
-    print ("ЭТА СТРОКА НЕ ЯВЛЯЕТСЯ ПАЛИНДРОМОМ")
-
-
-
-
+        print("Эти строки не являются анаграммами.")
+#задачо номир 3
+def udalitt_slova(s):
+    unique_bukvi = []
+    for char in s:
+        if s.count(char) == 1:
+            unique_bukvi.append(char)
+    return ''.join(unique_bukvi)
+if __name__ == "__main__":
+    input_string = input("Введите строку: ")
+    result = udalitt_slova(input_string)
+    print("Результат:", result)
 
 
